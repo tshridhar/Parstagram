e5cc34003f16bc21682335c340c16d8676fe5cab
Tuan Shridhar <tuan.shridhar@gmail.com>
2019-05-13 00:02:54 -0700

    all basic functionality completed.

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -23,11 +24,30 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSignIn(_ sender: Any) {
-    
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     @IBAction func onSignUp(_ sender: Any) {
-    
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     /*
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by KimNgan Nguyen on 5/12/19.
+//  Copyright © 2019 Tuan Shridhar. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
