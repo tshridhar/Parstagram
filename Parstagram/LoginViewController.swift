966e36a8430973e58b42751ddd85bfeac706f230
Tuan Shridhar <tuan.shridhar@gmail.com>
2019-05-12 22:45:44 -0700

    heavy progress, completed launch screen and login screen *minus* auto layout / constraints, and some functionality.

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignIn(_ sender: Any) {
+    
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    
+    }
+    
     /*
     // MARK: - Navigation
 
