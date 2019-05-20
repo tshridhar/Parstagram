252f8d3cc899a7b3b60fad628d3f4b49c4ceebde
Tuan Shridhar <tuan.shridhar@gmail.com>
2019-05-19 23:27:26 -0700

    nice

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by KimNgan Nguyen on 5/19/19.
+//  Copyright © 2019 Tuan Shridhar. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
